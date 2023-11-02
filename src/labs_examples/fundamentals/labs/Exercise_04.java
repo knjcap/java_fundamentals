181209d67c2658b74ba32d37a4de0254fa9d28e2
KNJCAP <knjcap@gmail.com>
2023-11-02 16:48:18 +0000

    completed exercise 04

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,34 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+      //  int i = 1;
+      //  System.out.println("int i is: " + i);
 
         // write your code below
+
+        int integerNumber = 11;
+        System.out.println("Khyle basketball number is " +integerNumber);
+
+        byte byteNumber = -14;
+        System.out.println("Your byte number is " +byteNumber);
+
+        short shortNumber = -31000;
+        System.out.println("Your short number is " +shortNumber);
+
+        long longNumber = 1000000000000L;
+        System.out.println("Your long number is " +longNumber);
+
+        float floatNumber = 23.34f;
+        System.out.println("Your float number is " +floatNumber);
+
+        double doubleNumber = 1.23;
+        System.out.println("Your double number is " +doubleNumber);
+
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " +isJavaFun);
+
+        char charNumber = 1;
+        System.out.println("Your char number is " +charNumber);
 
     }
 
