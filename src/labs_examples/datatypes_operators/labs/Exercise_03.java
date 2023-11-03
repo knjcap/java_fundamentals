7c4b08d3d0d933ded76f043dc4b86832822811f6
KNJCAP <knjcap@gmail.com>
2023-11-03 12:56:21 +0000

    completed datatypes_operators lab Exercice_03

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -10,7 +10,16 @@ class ArithmeticOperators {
 
     public static void main(String[] args) {
 
-        // write your code below
+        int a = 10 + 20;
+            System.out.println("10 + 20 = " +a);
+        int s = 23 - 9;
+            System.out.println("23 - 9 = " +s);
+        int m = 12 * 4;
+        System.out.println("12 * 4 = " +m);
+        int d = 16 / 4;
+            System.out.println("16 / 4 = " +d);
+        int mod = 14 % 6;
+            System.out.println("14 / 6 = 2 remainder "+mod);
 
     }
 
