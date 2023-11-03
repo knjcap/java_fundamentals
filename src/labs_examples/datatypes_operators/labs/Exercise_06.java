ca57d0301f561b19fa7851014e97fb2ae9959dbc
KNJCAP <knjcap@gmail.com>
2023-11-03 22:15:53 +0000

    completed datatypes_operators lab Exercice_06

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -11,6 +11,15 @@ package labs_examples.datatypes_operators.labs;
 public class Exercise_06 {
 
     public static void main(String[] args) {
+        float volume;
+        float surfaceArea;
+        float radius = 3.14f;
+        int height = 5;
+        float pi = 3.1416f;
+        volume = (pi * radius * radius * height); //formula is pi * radius to the 2nd power * height
+        System.out.println("The volume is " +volume);
+        surfaceArea = (2 * pi * radius * radius) + (2 * pi * radius * height); //formula is 2πr2 + 2πrh
+            System.out.println("The surface are is " +surfaceArea);
 
         // write code here
 
