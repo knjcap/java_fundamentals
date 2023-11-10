e938f41d4d2911619f581840b20862f9d4834efa
KNJCAP <knjcap@gmail.com>
2023-11-10 14:01:27 +0000

    src/labs_examples/conditions_loops/labs/Exercise_02.java

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -11,11 +11,34 @@ package labs_examples.conditions_loops.labs;
  *
  */
 
+
+import java.util.Scanner;
 public class Exercise_02 {
 
     public static void main(String[] args) {
     
         // write completed here
+    Scanner scanner = new Scanner (System.in);
+    System.out.print("Enter the number from 1 to 7: ");
+    int numberDay = scanner.nextInt();
+
+        if (numberDay ==1)
+            System.out.println("Monday");
+        else if (numberDay ==2)
+            System.out.println("Tuesday");
+        else if (numberDay ==3)
+            System.out.println("Wednesday");
+        else if (numberDay ==4)
+            System.out.println("Thursday");
+        else if (numberDay ==5)
+            System.out.println("Friday");
+        else if (numberDay ==6)
+            System.out.println("Saturday");
+        else if (numberDay ==7)
+            System.out.println("Sunday");
+        else
+            System.out.println("You have not entered the number from 1 to 7");
+
 
     }
 }
